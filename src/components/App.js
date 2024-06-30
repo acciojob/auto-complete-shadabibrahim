d930cf5178c31031c6fbe9258fd13f8518bdecee
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-06-30 20:30:05 +0530

    submitting code-Sun Jun 30 2024 20:30:04 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [search, setSearch] = useState("")
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" onChange={handleChange} />
+      <ul>
+        {fruits.filter((item) => item.toLowerCase().includes(search.toLowerCase()))
+          .map((item, index) => {
+            return <li key={index + 1}>{item}</li>
+          })}
+      </ul>
+
     </div>
   )
 }
